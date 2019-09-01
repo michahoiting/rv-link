e0cc58d0012754858a70d62b8922578b95eaf987
zoomdy <mingdu.zheng@gmail.com>
2019-09-01 18:53:44 +0800

    USB串口。

--- src/app/riscv-prober/main.c
+++ src/app/riscv-prober/main.c
@@ -1,3 +1,4 @@
+#if 0
 #include "rvl_jtag.h"
 #include "rvl_tap.h"
 
@@ -53,3 +54,68 @@ int main(void)
 
 	return 0;
 }
+#else
+
+#include "drv_usb_hw.h"
+#include "cdc_acm_core.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern uint8_t packet_sent, packet_receive;
+extern uint32_t receive_length;
+
+usb_core_driver USB_OTG_dev =
+{
+    .dev = {
+        .desc = {
+            .dev_desc       = (uint8_t *)&device_descriptor,
+            .config_desc    = (uint8_t *)&configuration_descriptor,
+            .strings        = usbd_strings,
+        }
+    }
+};
+
+
+/*!
+    \brief      main routine will construct a USB keyboard
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+int main(void)
+{
+    eclic_global_interrupt_enable();
+
+    eclic_priority_group_set(ECLIC_PRIGROUP_LEVEL2_PRIO2);
+
+    usb_rcu_config();
+
+    usb_timer_init();
+
+    usb_intr_config();
+
+    usbd_init (&USB_OTG_dev, USB_CORE_ENUM_FS, &usbd_cdc_cb);
+
+    /* check if USB device is enumerated successfully */
+    while (USBD_CONFIGURED != USB_OTG_dev.dev.cur_status) {
+    }
+
+    while (1) {
+        if (USBD_CONFIGURED == USB_OTG_dev.dev.cur_status) {
+            if (1 == packet_receive && 1 == packet_sent) {
+                packet_sent = 0;
+                /* receive datas from the host when the last packet datas have sent to the host */
+                cdc_acm_data_receive(&USB_OTG_dev);
+            } else {
+                if (0 != receive_length) {
+                    /* send receive datas */
+                    cdc_acm_data_send(&USB_OTG_dev, receive_length);
+                    receive_length = 0;
+                }
+            }
+        }
+    }
+}
+#endif
+

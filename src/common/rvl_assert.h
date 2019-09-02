b3046f9934ca845f1b4ded3f08603164c40fc488
zoomdy <mingdu.zheng@gmail.com>
2019-09-02 14:42:21 +0800

    RVL_ASSERT_EN宏控制断言的使用。

--- src/common/rvl_assert.h
+++ src/common/rvl_assert.h
@@ -1,9 +1,17 @@
 #ifndef __RVL_ASSERT_H__
 #define __RVL_ASSERT_H__
 
+#ifdef RVL_ASSERT_EN
+
 #define rvl_assert(cond) \
   do { \
     if(!(cond)) {for(;;) {} } \
   } while (0)
+
+#else
+
+#define rvl_assert(cond)
+
+#endif
 
 #endif /* __RVL_ASSERT_H__ */

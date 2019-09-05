f3eb5257002964e668251706bb67af1893e45926
zoomdy <mingdu.zheng@gmail.com>
2019-09-05 16:47:41 +0800

    添加 link 运行指示灯接口。

--- src/interface/led.h
+++ src/interface/led.h
@@ -2,6 +2,9 @@
 #define __INTERFACE_LED_H__
 
 void rvl_led_init(void);
-void rvl_led_gdb_connect(int connect);
+void rvl_led_gdb_connect(int on);
+void rvl_led_link_run(int on);
+void rvl_led_target_run(int on);
+void rvl_led_assert(int on);
 
 #endif /* __INTERFACE_LED_H__ */
--- src/link/gd32vf103c-start/rvl-led.c
+++ src/link/gd32vf103c-start/rvl-led.c
@@ -5,14 +5,21 @@
 #define LED_GDB_CONNECT_PORT    GPIOA
 #define LED_GDB_CONNECT_PIN     GPIO_PIN_1
 
+#define LED_LINK_RUN_PORT       GPIOA
+#define LED_LINK_RUN_PIN        GPIO_PIN_7
+
 
 void rvl_led_init(void)
 {
     rcu_periph_clock_enable(RCU_GPIOA);
 
-    gpio_init(LED_GDB_CONNECT_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, LED_GDB_CONNECT_PIN);
+    gpio_init(LED_GDB_CONNECT_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ, LED_GDB_CONNECT_PIN);
     gpio_bit_reset(LED_GDB_CONNECT_PORT, LED_GDB_CONNECT_PIN);
+
+    gpio_init(LED_LINK_RUN_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ, LED_LINK_RUN_PIN);
+    gpio_bit_reset(LED_LINK_RUN_PORT, LED_LINK_RUN_PIN);
 }
+
 
 void rvl_led_gdb_connect(int connect)
 {
@@ -20,5 +27,15 @@ void rvl_led_gdb_connect(int connect)
         gpio_bit_set(LED_GDB_CONNECT_PORT, LED_GDB_CONNECT_PIN);
     } else {
         gpio_bit_reset(LED_GDB_CONNECT_PORT, LED_GDB_CONNECT_PIN);
+    }
+}
+
+
+void rvl_led_link_run(int on)
+{
+    if (on) {
+        gpio_bit_set(LED_LINK_RUN_PORT, LED_LINK_RUN_PIN);
+    } else {
+        gpio_bit_reset(LED_LINK_RUN_PORT, LED_LINK_RUN_PIN);
     }
 }

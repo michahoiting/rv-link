3ce581d5c807e26e2a4f73b8b3b7554a7ea8c424
zoomdy <mingdu.zheng@gmail.com>
2019-09-05 16:49:16 +0800

    probe 应用现在可以通过虚拟串口打印信息了。

--- src/app/riscv-prober/main.c
+++ src/app/riscv-prober/main.c
@@ -1,20 +1,23 @@
 #include "pt.h"
+#include "interface/led.h"
+#include "interface/usb-serial.h"
 
-void task_jtag_init(void);
-PT_THREAD(task_jtag_poll(void));
-
-void task_usb_init(void);
-PT_THREAD(task_usb_poll(void));
+void task_probe_init(void);
+PT_THREAD(task_probe_poll(void));
 
 int main(void)
 {
-    task_jtag_init();
-    task_usb_init();
+    rvl_led_init();
+    usb_serial_init();
+    task_probe_init();
 
     for(;;) {
-        task_jtag_poll();
-        task_usb_poll();
+        usb_serial_recv_poll();
+        usb_serial_send_poll();
+        task_probe_poll();
     }
 
     return 0;
 }
+
+
--- src/app/riscv-prober/probe.c
+++ src/app/riscv-prober/probe.c
@@ -1,12 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "pt.h"
-#include "rvl-jtag.h"
+#include "interface/usb-serial.h"
+#include "interface/led.h"
 #include "rvl-tap.h"
 #include "v0p11/dtm.h"
 #include "v0p11/dmi.h"
 #include "rvl-assert.h"
 
 
-typedef struct task_jtag_s
+typedef struct task_probe_s
 {
     struct pt pt;
     int i;
@@ -28,91 +32,103 @@ typedef struct task_jtag_s
 
     uint32_t dmstatus;
     uint32_t dmi_result;
-}task_jtag_t;
 
-static task_jtag_t task_jtag_i;
+    char serial_buffer[1024];
+    bool serial_buffer_probe_own;
+    size_t serial_data_len;
 
-#define self task_jtag_i
+    bool probe_start;
+}task_probe_t;
 
-void task_jtag_init(void)
+static task_probe_t task_probe_i;
+#define self task_probe_i
+
+
+#define print(x...) \
+    do { \
+        PT_WAIT_UNTIL(&self.pt, self.serial_buffer_probe_own); \
+        snprintf(self.serial_buffer, sizeof(self.serial_buffer), x); \
+        self.serial_data_len = strlen(self.serial_buffer); \
+        self.serial_buffer_probe_own = false; \
+    }while(0);
+
+
+void task_probe_init(void)
 {
     PT_INIT(&self.pt);
+
+    self.serial_buffer_probe_own = true;
+    self.probe_start = false;
 }
 
-PT_THREAD(task_jtag_poll(void))
+
+PT_THREAD(task_probe_poll(void))
 {
     PT_BEGIN(&self.pt);
 
-#if 0
-    rvl_jtag_init();
-    PT_YIELD(&self.pt);
-#else
-    rvl_dtm_init();
+    PT_WAIT_UNTIL(&self.pt, self.probe_start);
+    rvl_led_link_run(1);
+
     rvl_dmi_init();
-#endif
 
-    rvl_tap_go_idle();
-    PT_YIELD(&self.pt);
-
-#if 0
-    // idcode, FE310-G000's idcode: 0x10e31913 (mfg: 0x489 (SiFive, Inc.), part: 0x0e31, ver: 0x1)
-    self.new_reg[0] = 0x01;
-    rvl_tap_shift_ir(self.old_reg, self.new_reg, 5);
-    PT_YIELD(&self.pt);
-
-    self.new_reg[0] = 0x00;
-    rvl_tap_shift_dr(self.idcode, self.new_reg, 32);
-    PT_YIELD(&self.pt);
-#else
+    // idcode
     for(self.i = 0; self.i < 3; self.i++) {
         PT_WAIT_THREAD(&self.pt, rvl_dtm_idcode(self.idcode));
-//        PT_YIELD(&self.pt);
     }
-#endif
 
-    rvl_assert(self.idcode[0] == 0x4e4796b);
+//    rvl_assert(self.idcode[0] == 0x4e4796b);
     self.idcode_version = self.idcode[0] >> 28;
     self.idcode_part_number = (self.idcode[0] >> 12) & 0xffff;
     self.idcode_manufid = (self.idcode[0] >> 1) & 0x7ff;
     self.idcode_lsb = self.idcode[0] & 0x1;
 
+    print("IDCODE: %08x\r\n", (int)self.idcode[0]);
+    print("DTM Version: %d\r\n", (int)self.idcode_version);
+
     // dtmcs
-
-#if 0
-    for(self.i = 0; self.i < 3; self.i++) {
-        self.new_reg[0] = 0x10;
-        rvl_tap_shift_ir(self.old_reg, self.new_reg, 5);
-        PT_YIELD(&self.pt);
-
-        self.new_reg[0] = 0x00;
-        rvl_tap_shift_dr(self.dtmcs, self.new_reg, 32);
-        PT_YIELD(&self.pt);
-    }
-#else
     for(self.i = 0; self.i < 3; self.i++) {
         PT_WAIT_THREAD(&self.pt, rvl_dtm_dtmcs(self.dtmcs));
-//        PT_YIELD(&self.pt);
     }
-#endif
 
-    rvl_assert(self.dtmcs[0] == 0x1450);
+//    rvl_assert(self.dtmcs[0] == 0x1450);
     self.dtmcs_version = self.dtmcs[0] & 0xf;
     self.dtmcs_abits = (self.dtmcs[0] >> 4) & 0x3f;
     self.dtmcs_dmistat = (self.dtmcs[0] >> 10) & 0x3;
     self.dtmcs_idle = (self.dtmcs[0] >> 12) & 0x7;
 
-//    PT_WAIT_THREAD(&self.pt, rvl_dtm_dtmcs_dmihardreset());
-
+    // dmstatus
     PT_WAIT_THREAD(&self.pt, rvl_dmi_read(0x11, &self.dmstatus, &self.dmi_result));
-    rvl_assert(self.dmstatus == 0x3c21);
-//    PT_YIELD(&self.pt);
+//    rvl_assert(self.dmstatus == 0x3c21);
 
-    //
-    for(;;) {
-//        rvl_tap_go_idle();
-//        PT_RESTART(&self.pt);
-        PT_YIELD(&self.pt);
-    }
+    rvl_led_link_run(0);
+    self.probe_start = false;
 
     PT_END(&self.pt);
+}
+
+
+bool usb_serial_put_recv_data(const uint8_t* buffer, size_t len)
+{
+    if(strncmp((char*)buffer, "start", 5) == 0) {
+        self.probe_start = true;
+    }
+
+    return true;
+}
+
+
+const uint8_t* usb_serial_get_send_data(size_t* len)
+{
+    if(self.serial_buffer_probe_own) {
+        return NULL;
+    } else {
+        *len = self.serial_data_len;
+        return (uint8_t*)self.serial_buffer;
+    }
+}
+
+
+void usb_serial_data_sent(void)
+{
+    self.serial_buffer_probe_own = true;
 }

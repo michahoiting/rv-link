be2790b209a9784b5109f76d66abb857674964b9
zoomdy <mingdu.zheng@gmail.com>
2019-09-13 10:52:49 +0800

    Fix：编译错误，缺少必要的头文件。

---
+++ src/target/family/gd32vf103/riscv-feature.h
@@ -0,0 +1,16 @@
+#ifndef __RVL_RISCV_FEATURE_H__
+#define __RVL_RISCV_FEATURE_H__
+
+#define RVL_TARGET_RISCV_FEATURE_CSR
+#define RVL_TARGET_RISCV_FEATURE_CSR_UMODE
+#define RVL_TARGET_RISCV_FEATURE_CSR_DMODE
+#define RVL_TARGET_RISCV_FEATURE_VIRTUAL
+
+#define RVL_TARGET_RISCV_FEATURE_CSR_CUSTOM \
+        "<reg name=\"pushmcause\" bitsize=\"32\" regnum=\"2030\" save-restore=\"no\" type=\"int\" group=\"csr\"/>" \
+        "<reg name=\"pushmepc\" bitsize=\"32\" regnum=\"2031\" save-restore=\"no\" type=\"int\" group=\"csr\"/>" \
+        "<reg name=\"sleepvalue\" bitsize=\"32\" regnum=\"2065\" save-restore=\"no\" type=\"int\" group=\"csr\"/>" \
+        "<reg name=\"txevt\" bitsize=\"32\" regnum=\"2066\" save-restore=\"no\" type=\"int\" group=\"csr\"/>" \
+        "<reg name=\"wfe\" bitsize=\"32\" regnum=\"2064\" save-restore=\"no\" type=\"int\" group=\"csr\"/>"
+
+#endif // __RVL_RISCV_FEATURE_H__
---
+++ src/target/family/riscv-debug-spec-v0p11/riscv-feature.h
@@ -0,0 +1,5 @@
+#ifndef __RVL_RISCV_FEATURE_H__
+#define __RVL_RISCV_FEATURE_H__
+
+
+#endif // __RVL_RISCV_FEATURE_H__

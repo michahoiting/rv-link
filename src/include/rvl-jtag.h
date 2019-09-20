15d06d7e904d8b67f956f891bf0f66962463ff9c
zoomdy <mingdu.zheng@gmail.com>
2019-09-20 09:02:47 +0800

    jtag 组件添加rvl_jtag_srst_put、rvl_jtag_delay接口。

--- src/include/rvl-jtag.h
+++ src/include/rvl-jtag.h
@@ -1,6 +1,8 @@
 #ifndef __INTERFACE_JTAG_H__
 #define __INTERFACE_JTAG_H__
 
+#include <stdint.h>
+
 void rvl_jtag_init(void);
 void rvl_jtag_fini(void);
 void rvl_jtag_tms_put(int tms);
@@ -8,6 +10,8 @@ void rvl_jtag_tdi_put(int tdi);
 void rvl_jtag_tck_put(int tck);
 void rvl_jtag_delay_half_period();
 int  rvl_jtag_tdo_get();
+void rvl_jtag_srst_put(int srst);
+void rvl_jtag_delay(uint32_t us);
 
 
 #endif /* __INTERFACE_JTAG_H__ */
--- src/link/gd32vf103c-start/rvl-jtag.c
+++ src/link/gd32vf103c-start/rvl-jtag.c
@@ -14,13 +14,17 @@
 #define TDO_PORT    GPIOB
 #define TDO_PIN     GPIO_PIN_12
 
+#define SRST_PORT   GPIOB
+#define SRST_PIN    GPIO_PIN_0
+
 void rvl_jtag_init(void)
 {
     rcu_periph_clock_enable(RCU_GPIOB);
     gpio_init(TCK_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, TCK_PIN);
     gpio_init(TMS_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, TMS_PIN);
     gpio_init(TDI_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, TDI_PIN);
-    gpio_init(TDO_PORT, GPIO_MODE_IPU, GPIO_OSPEED_10MHZ, TDO_PIN);
+    gpio_init(TDO_PORT, GPIO_MODE_IPU, 0, TDO_PIN);
+    gpio_init(SRST_PORT, GPIO_MODE_OUT_OD, GPIO_OSPEED_10MHZ, SRST_PIN);
 }
 
 
@@ -30,6 +34,7 @@ void rvl_jtag_fini(void)
     gpio_init(TMS_PORT, GPIO_MODE_AIN, 0, TMS_PIN);
     gpio_init(TDI_PORT, GPIO_MODE_AIN, 0, TDI_PIN);
     gpio_init(TDO_PORT, GPIO_MODE_AIN, 0, TDO_PIN);
+    gpio_init(SRST_PORT, GPIO_MODE_AIN, 0, SRST_PIN);
 }
 
 
@@ -78,4 +83,21 @@ int rvl_jtag_tdo_get()
     }
 
     return tdo;
+}
+
+void rvl_jtag_srst_put(int srst)
+{
+    if (srst) {
+        gpio_bit_set(SRST_PORT, SRST_PIN);
+    } else {
+        gpio_bit_reset(SRST_PORT, SRST_PIN);
+    }
+}
+
+void rvl_jtag_delay(uint32_t us)
+{
+    uint32_t start = read_csr(mcycle);
+    uint32_t period = us * 96; // CPU run @96MHz
+
+    while(read_csr(mcycle) - start < period);
 }

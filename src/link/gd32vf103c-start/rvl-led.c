16e65bd8edf1a3b72b84c396b296f3da611d42af
zoomdy <mingdu.zheng@gmail.com>
2019-09-23 21:03:53 +0800

    根据运行效果进行调整。

--- src/link/gd32vf103c-start/rvl-led.c
+++ src/link/gd32vf103c-start/rvl-led.c
@@ -20,8 +20,8 @@ static rvl_led_t rvl_led_i;
 #define LED_GDB_CONNECT_PORT    GPIOA
 #define LED_GDB_CONNECT_PIN     GPIO_PIN_1
 
-#define LED_LINK_RUN_PORT       GPIOA
-#define LED_LINK_RUN_PIN        GPIO_PIN_7
+#define LED1_PORT               GPIOA
+#define LED1_PIN                GPIO_PIN_7
 
 
 void rvl_led_init(void)
@@ -35,32 +35,33 @@ void rvl_led_init(void)
     gpio_init(LED_GDB_CONNECT_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ, LED_GDB_CONNECT_PIN);
     gpio_bit_reset(LED_GDB_CONNECT_PORT, LED_GDB_CONNECT_PIN);
 
-    gpio_init(LED_LINK_RUN_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ, LED_LINK_RUN_PIN);
-    gpio_bit_reset(LED_LINK_RUN_PORT, LED_LINK_RUN_PIN);
+    gpio_init(LED1_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ, LED1_PIN);
+    gpio_bit_reset(LED1_PORT, LED1_PIN);
 }
 
 
-#define CYCLES_PER_POLL     20
 PT_THREAD(rvl_led_poll(void))
 {
     PT_BEGIN(&self.pt);
 
-    if(self.target_run) {
-        self.on_period = 10 * 1000 * 1000 / CYCLES_PER_POLL;
-        self.off_period = 10 * 1000 * 1000 / CYCLES_PER_POLL;
-    } else if(self.gdb_connect) {
-        self.on_period = 50 * 1000 * 1000 / CYCLES_PER_POLL;
-        self.off_period = 50 * 1000 * 1000 / CYCLES_PER_POLL;
+    if(self.gdb_connect) {
+        if(self.target_run) {
+            self.on_period = 10 * 1000 * 1000;
+            self.off_period = 10 * 1000 * 1000;
+        } else {
+            self.on_period = 50 * 1000 * 1000;
+            self.off_period = 50 * 1000 * 1000;
+        }
     } else {
-        self.on_period = 10 * 1000 * 1000 / CYCLES_PER_POLL;
-        self.off_period = 100 * 1000 * 1000 / CYCLES_PER_POLL;
+        self.on_period = 10 * 1000 * 1000;
+        self.off_period = 100 * 1000 * 1000;
     }
 
-    gpio_bit_set(LED_LINK_RUN_PORT, LED_LINK_RUN_PIN);
+    gpio_bit_set(LED1_PORT, LED1_PIN);
     self.mcycle_start = read_csr(mcycle);
     PT_WAIT_UNTIL(&self.pt, read_csr(mcycle) - self.mcycle_start >= self.on_period);
 
-    gpio_bit_reset(LED_LINK_RUN_PORT, LED_LINK_RUN_PIN);
+    gpio_bit_reset(LED1_PORT, LED1_PIN);
     self.mcycle_start = read_csr(mcycle);
     PT_WAIT_UNTIL(&self.pt, read_csr(mcycle) - self.mcycle_start >= self.off_period);
 
@@ -83,9 +84,9 @@ void rvl_led_gdb_connect(int connect)
 void rvl_led_link_run(int on)
 {
     if (on) {
-        gpio_bit_set(LED_LINK_RUN_PORT, LED_LINK_RUN_PIN);
+        gpio_bit_set(LED1_PORT, LED1_PIN);
     } else {
-        gpio_bit_reset(LED_LINK_RUN_PORT, LED_LINK_RUN_PIN);
+        gpio_bit_reset(LED1_PORT, LED1_PIN);
     }
 }
 
@@ -99,8 +100,8 @@ void rvl_led_target_run(int on)
 void rvl_led_assert(int on)
 {
     if (on) {
-        gpio_bit_set(LED_LINK_RUN_PORT, LED_LINK_RUN_PIN);
+        gpio_bit_set(LED1_PORT, LED1_PIN);
     } else {
-        gpio_bit_reset(LED_LINK_RUN_PORT, LED_LINK_RUN_PIN);
+        gpio_bit_reset(LED1_PORT, LED1_PIN);
     }
 }
--- src/link/longan-nano/rvl-led.c
+++ src/link/longan-nano/rvl-led.c
@@ -18,14 +18,14 @@ static rvl_led_t rvl_led_i;
 #define self rvl_led_i
 
 
-#define LED_GDB_CONNECT_PORT    GPIOA
-#define LED_GDB_CONNECT_PIN     GPIO_PIN_2
+#define LED_BLUE_PORT           GPIOA
+#define LED_BLUE_PIN            GPIO_PIN_2
 
-#define LED_TARGET_RUN_PORT     GPIOA
-#define LED_TARGET_RUN_PIN      GPIO_PIN_1
+#define LED_GREEN_PORT          GPIOA
+#define LED_GREEN_PIN           GPIO_PIN_1
 
-#define LED_ASSERT_PORT         GPIOC
-#define LED_ASSERT_PIN          GPIO_PIN_13
+#define LED_RED_PORT            GPIOC
+#define LED_RED_PIN             GPIO_PIN_13
 
 
 void rvl_led_init(void)
@@ -37,38 +37,39 @@ void rvl_led_init(void)
     rcu_periph_clock_enable(RCU_GPIOA);
     rcu_periph_clock_enable(RCU_GPIOC);
 
-    gpio_init(LED_GDB_CONNECT_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ, LED_GDB_CONNECT_PIN);
-    gpio_bit_set(LED_GDB_CONNECT_PORT, LED_GDB_CONNECT_PIN);
+    gpio_init(LED_BLUE_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ, LED_BLUE_PIN);
+    gpio_bit_set(LED_BLUE_PORT, LED_BLUE_PIN);
 
-    gpio_init(LED_TARGET_RUN_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ, LED_TARGET_RUN_PIN);
-    gpio_bit_set(LED_TARGET_RUN_PORT, LED_TARGET_RUN_PIN);
+    gpio_init(LED_GREEN_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ, LED_GREEN_PIN);
+    gpio_bit_set(LED_GREEN_PORT, LED_GREEN_PIN);
 
-    gpio_init(LED_ASSERT_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ, LED_ASSERT_PIN);
-    gpio_bit_set(LED_ASSERT_PORT, LED_ASSERT_PIN);
+    gpio_init(LED_RED_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ, LED_RED_PIN);
+    gpio_bit_set(LED_RED_PORT, LED_RED_PIN);
 }
 
 
-#define CYCLES_PER_POLL     20
 PT_THREAD(rvl_led_poll(void))
 {
     PT_BEGIN(&self.pt);
 
-    if(self.target_run) {
-        self.on_period = 10 * 1000 * 1000 / CYCLES_PER_POLL;
-        self.off_period = 10 * 1000 * 1000 / CYCLES_PER_POLL;
-    } else if(self.gdb_connect) {
-        self.on_period = 50 * 1000 * 1000 / CYCLES_PER_POLL;
-        self.off_period = 50 * 1000 * 1000 / CYCLES_PER_POLL;
+    if(self.gdb_connect) {
+        if(self.target_run) {
+            self.on_period = 10 * 1000 * 1000;
+            self.off_period = 10 * 1000 * 1000;
+        } else {
+            self.on_period = 50 * 1000 * 1000;
+            self.off_period = 50 * 1000 * 1000;
+        }
     } else {
-        self.on_period = 10 * 1000 * 1000 / CYCLES_PER_POLL;
-        self.off_period = 100 * 1000 * 1000 / CYCLES_PER_POLL;
+        self.on_period = 10 * 1000 * 1000;
+        self.off_period = 100 * 1000 * 1000;
     }
 
-    gpio_bit_set(LED_GDB_CONNECT_PORT, LED_GDB_CONNECT_PIN);
+    gpio_bit_reset(LED_GREEN_PORT, LED_GREEN_PIN);
     self.mcycle_start = read_csr(mcycle);
     PT_WAIT_UNTIL(&self.pt, read_csr(mcycle) - self.mcycle_start >= self.on_period);
 
-    gpio_bit_reset(LED_GDB_CONNECT_PORT, LED_GDB_CONNECT_PIN);
+    gpio_bit_set(LED_GREEN_PORT, LED_GREEN_PIN);
     self.mcycle_start = read_csr(mcycle);
     PT_WAIT_UNTIL(&self.pt, read_csr(mcycle) - self.mcycle_start >= self.off_period);
 
@@ -82,12 +83,22 @@ void rvl_led_gdb_connect(int connect)
 }
 
 
+void rvl_led_link_run(int on)
+{
+    if (on) {
+        gpio_bit_set(LED_GREEN_PORT, LED_GREEN_PIN);
+    } else {
+        gpio_bit_reset(LED_GREEN_PORT, LED_GREEN_PIN);
+    }
+}
+
+
 void rvl_led_target_run(int on)
 {
     if (!on) {
-        gpio_bit_set(LED_TARGET_RUN_PORT, LED_TARGET_RUN_PIN);
+        gpio_bit_set(LED_BLUE_PORT, LED_BLUE_PIN);
     } else {
-        gpio_bit_reset(LED_TARGET_RUN_PORT, LED_TARGET_RUN_PIN);
+        gpio_bit_reset(LED_BLUE_PORT, LED_BLUE_PIN);
     }
 
     self.target_run = (bool)on;
@@ -97,8 +108,8 @@ void rvl_led_target_run(int on)
 void rvl_led_assert(int on)
 {
     if (!on) {
-        gpio_bit_set(LED_ASSERT_PORT, LED_ASSERT_PIN);
+        gpio_bit_set(LED_RED_PORT, LED_RED_PIN);
     } else {
-        gpio_bit_reset(LED_ASSERT_PORT, LED_ASSERT_PIN);
+        gpio_bit_reset(LED_RED_PORT, LED_RED_PIN);
     }
 }

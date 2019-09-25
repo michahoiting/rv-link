22fc2e4f69a49149c63ee1f95599fb8ac8427020
zoomdy <mingdu.zheng@gmail.com>
2019-09-25 14:09:09 +0800

    实现 Protothreads 的定时器，计时单位为微秒。

---
+++ src/link/gd32vf103c-start/pt-clock.c
@@ -0,0 +1,21 @@
+#include "clock.h"
+#include "riscv_encoding.h"
+
+
+clock_time_t clock_time(void)
+{
+    uint32_t mc, mch1, mch2;
+    uint64_t cycle;
+    uint32_t us;
+
+    do {
+        mch1 = read_csr(mcycleh);
+        mc = read_csr(mcycle);
+        mch2 = read_csr(mcycleh);
+    } while(mch1 != mch2);
+
+    cycle = ((uint64_t)mch2 << 32) | mc;
+    us = (uint32_t)(cycle / (uint64_t)96);
+
+    return us;
+}

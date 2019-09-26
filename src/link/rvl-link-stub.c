5cdafc5c616eeab663b4f68230be702c878483cf
zoomdy <mingdu.zheng@gmail.com>
2019-09-26 09:46:55 +0800

    __attribute__((weak)) 放在函数定义的前面就可以，不需要单独声明。

--- src/link/rvl-link-stub.c
+++ src/link/rvl-link-stub.c
@@ -3,57 +3,49 @@
 #include "pt.h"
 
 
-void rvl_button_init(void) __attribute__((weak));
-void rvl_button_init(void)
+__attribute__((weak)) void rvl_button_init(void)
 {
 
 }
 
 
-int rvl_button_pushed(void) __attribute__((weak));
-int rvl_button_pushed(void)
+__attribute__((weak)) int rvl_button_pushed(void)
 {
     return 0;
 }
 
 
-void rvl_led_init(void) __attribute__((weak));
-void rvl_led_init(void)
+__attribute__((weak)) void rvl_led_init(void)
 {
 
 }
 
 
-PT_THREAD(rvl_led_poll(void)) __attribute__((weak));
-PT_THREAD(rvl_led_poll(void))
+__attribute__((weak)) PT_THREAD(rvl_led_poll(void))
 {
     return PT_ENDED;
 }
 
 
-void rvl_led_gdb_connect(int on) __attribute__((weak));
-void rvl_led_gdb_connect(int on)
+__attribute__((weak)) void rvl_led_gdb_connect(int on)
 {
 
 }
 
 
-void rvl_led_link_run(int on) __attribute__((weak));
-void rvl_led_link_run(int on)
+__attribute__((weak)) void rvl_led_link_run(int on)
 {
 
 }
 
 
-void rvl_led_target_run(int on) __attribute__((weak));
-void rvl_led_target_run(int on)
+__attribute__((weak)) void rvl_led_target_run(int on)
 {
 
 }
 
 
-void rvl_led_assert(int on) __attribute__((weak));
-void rvl_led_assert(int on)
+__attribute__((weak)) void rvl_led_assert(int on)
 {
 
 }

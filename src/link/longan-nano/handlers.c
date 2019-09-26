8d568efa5c9ef21075ce48e8349e58b50f34d7a2
zoomdy <mingdu.zheng@gmail.com>
2019-09-26 09:57:11 +0800

    如果RV-LINK自身发生了程序异常，那么快闪红色LED。

---
+++ src/link/longan-nano/handlers.c
@@ -0,0 +1,24 @@
+#include <stdint.h>
+
+#include "rvl-led.h"
+#include "timer.h"
+
+
+uintptr_t handle_trap(uintptr_t mcause, uintptr_t sp)
+{
+    struct timer timer;
+
+    timer_set(&timer, 100 * 1000 /* 100 ms */);
+
+    for(;;) {
+        rvl_led_assert(1);
+        while(!timer_expired(&timer));
+        timer_reset(&timer);
+
+        rvl_led_assert(0);
+        while(!timer_expired(&timer));
+        timer_reset(&timer);
+    }
+
+    return 0;
+}

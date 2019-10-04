af9bad55fba9dc7a96294e6950aebfbe3d471446
zoomdy <mingdu.zheng@gmail.com>
2019-10-04 18:46:13 +0800

    合并 rvl-tap-config.h 和 riscv-feature.h 中的配置宏定义到 rvl-target-config.h 中。

--- src/target/family/riscv-debug-spec-v0p11/rvl-tap-config.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RVL_TAP_CONFIG_H__
-#define __RVL_TAP_CONFIG_H__
-
-#define RVL_TAP_CONFIG_DYN
-
-#endif // __RVL_TAP_CONFIG_H__
-
--- src/target/family/riscv-debug-spec-v0p13/riscv-feature.h
+++
@@ -1,5 +0,0 @@
-#ifndef __RVL_RISCV_FEATURE_H__
-#define __RVL_RISCV_FEATURE_H__
-
-
-#endif // __RVL_RISCV_FEATURE_H__

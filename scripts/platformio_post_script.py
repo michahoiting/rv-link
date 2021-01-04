ef306050994eded8b94bb17927e69e4531feb0bf
Micha Hoiting <micha.hoiting@gmail.com>
2021-01-04 16:52:34 +0100

    Added disassembler output and renamed the fw filename to contain the board name.

---
+++ scripts/platformio_post_script.py
@@ -0,0 +1,30 @@
+Import("env", "projenv")
+
+env.Replace(
+    OBJDUMP="riscv-nuclei-elf-objdump"
+)
+
+
+# Custom disassemble from ELF target
+env.AddPostAction(
+    "$BUILD_DIR/${PROGNAME}.elf",
+    env.VerboseAction(" ".join([
+        "$OBJDUMP", "--disassemble-all", "--source ",
+        "${TARGET}", "> ${TARGET}.S"
+    ]), "Building ${TARGET}.S")
+)
+
+
+def dump_env_to_file(source, target, env):
+    '''Dump PlatformIO global, project construction and build environment to file'''
+    print(source[0])
+    print(target[0])
+    env_filename = env.File("$BUILD_DIR/${PROGNAME}.env.txt")
+    env_file = open(str(env_filename), "w")
+    env_file.write("env\n---\n" + str(env))
+    env_file.write("\n\nprojenv\n-------\n" + str(projenv))
+    env_file.write("\n\nenv.Dump()\n----------\n" + str(env.Dump()))
+    env_file.write("\n\nprojenv.Dump()\n--------------\n" + str(projenv.Dump()))
+    env_file.close()
+
+env.AddPostAction("$BUILD_DIR/${PROGNAME}.elf", dump_env_to_file)

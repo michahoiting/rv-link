a921101a16bb339e128218f103ec6b48035d1a30
Micha Hoiting <micha.hoiting@gmail.com>
2021-01-22 22:49:10 +0100

    Update gitlab-publish.sh

--- scripts/gitlab-publish.sh
+++ scripts/gitlab-publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Description: Publish RV-Link packages
-#              Atleast an empty hook is needed for GitLan:
+#              Atleast an empty hook is needed for GitLab:
 #              - jobs:publish config should contain either a trigger or a
 #              - needs:pipeline
 

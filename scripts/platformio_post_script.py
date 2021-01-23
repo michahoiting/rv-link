665555d77e269e936c4b951c58cd214172996e76
Micha Hoiting <micha.hoiting@gmail.com>
2021-01-23 12:43:21 +0100

    Separated out the build per board.

--- scripts/platformio_post_script.py
+++ scripts/platformio_post_script.py
@@ -1,19 +1,40 @@
+from os.path import join
+
+from SCons.Script import (ARGUMENTS, COMMAND_LINE_TARGETS, AlwaysBuild,
+                          Default, DefaultEnvironment)
+
 Import("env", "projenv")
 
 env.Replace(
     OBJDUMP="riscv-nuclei-elf-objdump"
 )
 
-
-# Custom disassemble from ELF target
+# Custom BIN from ELF
 env.AddPostAction(
     "$BUILD_DIR/${PROGNAME}.elf",
     env.VerboseAction(" ".join([
-        "$OBJDUMP", "--disassemble-all", "--source ",
-        "${TARGET}", "> ${TARGET}.S"
-    ]), "Building ${TARGET}.S")
+        "$OBJCOPY", "-O", "binary",
+        "$BUILD_DIR/${PROGNAME}.elf", "$BUILD_DIR/${PROGNAME}.bin"
+    ]), "Building $BUILD_DIR/${PROGNAME}.bin")
 )
 
+# Custom HEX from ELF
+env.AddPostAction(
+    "$BUILD_DIR/${PROGNAME}.elf",
+    env.VerboseAction(" ".join([
+        "$OBJCOPY", "-O", "ihex",
+        "$BUILD_DIR/${PROGNAME}.elf", "$BUILD_DIR/${PROGNAME}.hex"
+    ]), "Building $BUILD_DIR/${PROGNAME}.hex")
+)
+
+# Custom disassemble from ELF
+env.AddPostAction(
+    "$BUILD_DIR/${PROGNAME}.elf",
+    env.VerboseAction(" ".join([
+        "$OBJDUMP", "--disassemble-all", "--source",
+        "$BUILD_DIR/${PROGNAME}.elf", "> $BUILD_DIR/${PROGNAME}.S"
+    ]), "Building $BUILD_DIR/${PROGNAME}.S")
+)
 
 def dump_env_to_file(source, target, env):
     '''Dump PlatformIO global, project construction and build environment to file'''
@@ -27,4 +48,5 @@ def dump_env_to_file(source, target, env):
     env_file.write("\n\nprojenv.Dump()\n--------------\n" + str(projenv.Dump()))
     env_file.close()
 
+# Uncomment to dump the PlatformIO environment to file
 #env.AddPostAction("$BUILD_DIR/${PROGNAME}.elf", dump_env_to_file)
